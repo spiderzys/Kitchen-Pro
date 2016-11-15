6453f99b333a15b26750497099a0894ca9d17a0f
spiderzys <spiderzys@hotmail.com>
2016-11-15 16:26:28 -0500

    Initial Commit

---
+++ Kitchen Pro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kitchen Pro
+//
+//  Created by YANGSHENG ZOU on 2016-11-15.
+//  Copyright © 2016 YANGSHENG ZOU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
